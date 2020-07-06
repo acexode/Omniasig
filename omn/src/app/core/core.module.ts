11b0894372562ed95d1e883a96c2726b8f1132c0
maxciceu <maxciceu2@gmail.com>
2020-07-06 18:19:44 +0300

    Update core services

--- omn/src/app/core/core.module.ts
+++ omn/src/app/core/core.module.ts
@@ -5,16 +5,26 @@ import { fakeBackendProvider } from './interceptors/fake-backend';
 import { ConfigService } from './services/config/config.service';
 import { RequestService } from './services/request/request.service';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { IonicStorageModule } from '@ionic/storage';
+import { CustomStorageService } from './services/custom-storage/custom-storage.service';
+import { AuthService } from './services/auth/auth.service';
 
 @NgModule({
   declarations: [SideMenuComponent],
-  imports: [CommonModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    IonicStorageModule.forRoot({
+      name: '__omndb',
+      driverOrder: ['sqlite', 'indexeddb', 'localstorage', 'websql'],
+    }),
+  ],
   providers: [
     fakeBackendProvider,
     RequestService,
     ConfigService,
-    // AuthService,
-    // CustomStorageService,
+    AuthService,
+    CustomStorageService,
   ],
   exports: [SideMenuComponent],
 })
--- omn/src/app/core/services/custom-storage/custom-storage.service.ts
+++ omn/src/app/core/services/custom-storage/custom-storage.service.ts
@@ -1,77 +1,29 @@
-// import { Injectable } from '@angular/core';
-// import { Observable, from } from 'rxjs';
-// import {
-//   CachedItem,
-//   DedicatedInstanceFactory,
-//   Driver,
-//   NgForage,
-//   NgForageCache,
-// } from 'ngforage';
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { from, Observable } from 'rxjs';
 
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class CustomStorageService {
-//   private readonly ngf: NgForage;
-//   private readonly cache: NgForageCache;
-//   constructor(dif: DedicatedInstanceFactory) {
-//     const conf = {
-//       name: 'omna-auth',
-//       driver: [
-//         // defaults to indexedDB -> webSQL -> localStorage
-//         Driver.INDEXED_DB,
-//         Driver.LOCAL_STORAGE,
-//       ],
-//     };
-//     this.ngf = dif.createNgForage(conf);
-//     this.cache = dif.createCache(conf);
-//   }
+@Injectable({
+  providedIn: 'root',
+})
+export class CustomStorageService {
+  constructor(private storage: Storage) {}
 
-//   public getItem<T = any>(key: string, ngf = this.ngf): Observable<T> {
-//     return from(ngf.getItem<T>(key));
-//   }
+  public getItem<T>(key: string): Observable<T> {
+    return from(this.storage.get(key));
+  }
+  public setItem<T>(key: string, data: T): Observable<T> {
+    return from(this.storage.set(key, data));
+  }
 
-//   public getCachedItem<T = any>(
-//     key: string,
-//     ngc = this.cache
-//   ): Observable<T | null> {
-//     return from(
-//       ngc.getCached<T>(key).then((r: CachedItem<T>) => {
-//         if (!r.hasData || r.expired) {
-//           return null;
-//         }
-//         return r.data;
-//       })
-//     );
-//   }
-//   public setItem<T = any>(key: string, data: T, ngf = this.ngf): Observable<T> {
-//     return from(ngf.setItem<T>(key, data));
-//   }
+  public removeItem(key: string) {
+    this.storage.remove(key);
+  }
 
-//   public setCachedItem<T = any>(
-//     key: string,
-//     data: T,
-//     cacheTime?: number,
-//     ngc = this.cache
-//   ): Observable<T | null> {
-//     return from(ngc.setCached<T>(key, data, cacheTime));
-//   }
+  public keys(): Observable<string[]> {
+    return from(this.storage.keys());
+  }
 
-//   public removeCached(key: string, ngc = this.cache) {
-//     ngc.removeCached(key);
-//   }
-
-//   public removeItem(key: string, s = this.ngf) {
-//     s.removeItem(key);
-//   }
-
-//   public keys(s = this.ngf): Observable<string[]> {
-//     return from(s.keys());
-//   }
-
-//   public clear(s = this.ngf): Observable<void> {
-//     return from(s.clear());
-//   }
-// }
-
-// TODO: Extend this to use Ionic's storage, similar to what ngforage does.
+  public clear(): Observable<void> {
+    return from(this.storage.clear());
+  }
+}
