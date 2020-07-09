483d5ef7c97a8e50a15c64a632a6b9b8bc330585
maxciceu <maxciceu2@gmail.com>
2020-07-09 14:15:01 +0300

    Update main page

--- omn/src/app/shared/components/tab-menu/tab-menu.component.ts
+++ omn/src/app/shared/components/tab-menu/tab-menu.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-tab-menu",
-  templateUrl: "./tab-menu.component.html",
-  styleUrls: ["./tab-menu.component.scss"],
+  selector: 'app-tab-menu',
+  templateUrl: './tab-menu.component.html',
+  styleUrls: ['./tab-menu.component.scss'],
 })
 export class TabMenuComponent implements OnInit {
   currentTab: string;
--- omn/src/app/shared/shared.module.ts
+++ omn/src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { TabMenuComponent } from './components/tab-menu/tab-menu.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [TabMenuComponent],
-  imports: [CommonModule, FormsModule, IonicModule],
+  imports: [CommonModule, FormsModule, IonicModule, RouterModule],
   exports: [TabMenuComponent],
 })
 export class SharedModule {}
