15eaa2bfda0d998ac390580efbf245719abf4b68
Samson Nwokike <samson.ikechukwu@softescu.com>
2020-07-14 15:36:13 +0100

    add active variable for dynamic content display

--- omn/src/app/profile/pages/date-personale/date-personale.page.ts
+++ omn/src/app/profile/pages/date-personale/date-personale.page.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-date-personale',
-  templateUrl: './date-personale.page.html',
-  styleUrls: ['./date-personale.page.scss'],
+  selector: "app-date-personale",
+  templateUrl: "./date-personale.page.html",
+  styleUrls: ["./date-personale.page.scss"],
 })
 export class DatePersonalePage implements OnInit {
+  active: boolean = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
