4f13340989ef98d6b7c2f7f90bafe4e6c6dd6a58
Samson Nwokike <samson.ikechukwu@softescu.com>
2020-07-15 15:33:18 +0100

    fix merging issues

--- omn/src/app/shared/shared.module.ts
+++ omn/src/app/shared/shared.module.ts
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
-import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { IonicModule } from "@ionic/angular";
+import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { IonicModule } from "@ionic/angular";
+import { DisabledPlaceholderComponent } from "./components/disabled-placeholder/disabled-placeholder.component";
+import { ImageCardComponent } from "./components/image-card/image-card.component";
+import { PolicyListComponent } from "./components/policy-list/policy-list.component";
 import { TabMenuComponent } from "./components/tab-menu/tab-menu.component";
 import { SubPageHeaderComponent } from "./components/sub-page-header/sub-page-header.component";
-
-@NgModule({
-  declarations: [TabMenuComponent, SubPageHeaderComponent],
-  imports: [CommonModule, FormsModule, IonicModule],
-  exports: [TabMenuComponent, SubPageHeaderComponent],
-=======
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { IonicModule } from '@ionic/angular';
-import { DisabledPlaceholderComponent } from './components/disabled-placeholder/disabled-placeholder.component';
-import { ImageCardComponent } from './components/image-card/image-card.component';
-import { PolicyListComponent } from './components/policy-list/policy-list.component';
-import { TabMenuComponent } from './components/tab-menu/tab-menu.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +15,7 @@ import { TabMenuComponent } from './components/tab-menu/tab-menu.component';
     ImageCardComponent,
     DisabledPlaceholderComponent,
     PolicyListComponent,
+    SubPageHeaderComponent,
   ],
   imports: [CommonModule, FormsModule, IonicModule, RouterModule],
   exports: [
@@ -34,7 +23,7 @@ import { TabMenuComponent } from './components/tab-menu/tab-menu.component';
     ImageCardComponent,
     DisabledPlaceholderComponent,
     PolicyListComponent,
+    SubPageHeaderComponent,
   ],
->>>>>>> f6ea9ace60478f55e9ce5a51240071e130086bba
 })
 export class SharedModule {}
