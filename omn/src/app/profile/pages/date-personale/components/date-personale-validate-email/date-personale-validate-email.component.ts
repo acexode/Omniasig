7ab7d9796a741da2f8d4b556703254131b8e34cb
maxciceu <maxciceu2@gmail.com>
2020-07-16 16:40:08 +0300

    Action sheet styling

--- omn/src/app/profile/pages/date-personale/components/date-personale-validate-email/date-personale-validate-email.component.ts
+++ omn/src/app/profile/pages/date-personale/components/date-personale-validate-email/date-personale-validate-email.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-date-personale-validate-email',
@@ -7,9 +8,38 @@ import { Component, HostBinding, OnInit } from '@angular/core';
 })
 export class DatePersonaleValidateEmailComponent implements OnInit {
   @HostBinding('class') color = 'ion-color-white-page';
-  constructor() {}
+  constructor(public actionSheetController: ActionSheetController) {}
 
   ngOnInit() {}
 
-  openVerifyModal() {}
+  async openVerifyModal() {
+    let actionSheet = null;
+    this.actionSheetController
+      .create({
+        header: 'Verifică inbox',
+        buttons: [
+          {
+            text: 'Deschide e-mail',
+            cssClass:
+              'm-0 w-100 no-shadow ion-color text-weight-medium ion-color-success button button-block button-large button-solid',
+            handler: () => {
+              console.log('Share clicked');
+            },
+          },
+          {
+            text: 'Renunță',
+            role: 'cancel',
+            cssClass:
+              'm-0 w-100 no-shadow ion-color-secondary button button-block button-large button-solid',
+            handler: () => {
+              console.log('Cancel clicked');
+            },
+          },
+        ],
+      })
+      .then((v) => {
+        actionSheet = v;
+        actionSheet.present();
+      });
+  }
 }
