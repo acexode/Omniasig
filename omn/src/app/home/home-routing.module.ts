bcea382c654a8a4d86683751264f82e1a055eafb
Samson Nwokike <samson.ikechukwu@softescu.com>
2020-07-17 07:37:30 +0100

    fix profil routing

--- omn/src/app/home/home-routing.module.ts
+++ omn/src/app/home/home-routing.module.ts
@@ -1,18 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomePage } from './home.page';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomePage } from "./home.page";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: HomePage,
-    children: [
-      {
-        path: 'profile',
-        loadChildren: () =>
-          import('../profile/profile.module').then((m) => m.ProfilePageModule),
-      },
-    ],
   },
 ];
 
