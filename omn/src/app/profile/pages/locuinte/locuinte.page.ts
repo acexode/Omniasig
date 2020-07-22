432722949698df9beff16a0aca627dfd9079a9c2
Samson Nwokike <samson.ikechukwu@softescu.com>
2020-07-22 08:20:40 +0100

    locuinte content

--- omn/src/app/profile/pages/locuinte/locuinte.page.ts
+++ omn/src/app/profile/pages/locuinte/locuinte.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { subPageHeaderDefault } from 'src/app/shared/data/sub-page-header-default';
+import { ImageCard } from 'src/app/shared/models/component/image-card';
 
 @Component({
   selector: 'app-locuinte',
@@ -8,6 +9,24 @@ import { subPageHeaderDefault } from 'src/app/shared/data/sub-page-header-defaul
 })
 export class LocuintePage implements OnInit {
   headerConfig = subPageHeaderDefault('Locuințe');
+  cards: Array<ImageCard> = [
+    {
+      mainIcon: {
+        name: 'md-pin-ling',
+        color: 'green-gradient',
+        classes: 'icon-40 mt-16 mb-8',
+      },
+      textContent: [
+        {
+          text: 'Acasă Traian 45, Brasov',
+        },
+      ],
+      id: 'places',
+      itemClass: 'mh-104 shadow-page-item',
+      isButton: true,
+      // routerLink: ['locuinte'],
+    },
+  ];
   constructor() {}
 
   ngOnInit() {}
