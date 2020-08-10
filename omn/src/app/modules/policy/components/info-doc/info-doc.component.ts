7af092ca5faca772a8cc4023782d256bf002235d
Samson Nwokike <samson.ikechukwu@softescu.com>
2020-08-10 06:07:51 +0100

    customize list icons

--- omn/src/app/modules/policy/components/info-doc/info-doc.component.ts
+++ omn/src/app/modules/policy/components/info-doc/info-doc.component.ts
@@ -105,18 +105,19 @@ export class InfoDocComponent implements OnInit {
         text: 'Ce se asigură?',
         classes: '',
       },
+      listClass: 'green',
       textContents: [
-        `✔︎ Prin PAD se asigură exclusiv construcțiile cu destinația de locuință
+        `Prin PAD se asigură exclusiv construcțiile cu destinația de locuință
         pentru următoarele trei riscuri: cutremure de pământ, alunecări de teren
         și inundații, ca fenomene naturale.`,
-        `✔︎ În cazul producerii unui risc asigurat se acordă despăgubiri și pentru:`,
-        ` ⁃ daunele directe provocate locuinței, consecința indirectă a producerii
+        `În cazul producerii unui risc asigurat se acordă despăgubiri și pentru:`,
+        `daunele directe provocate locuinței, consecința indirectă a producerii
         riscurilor asigurate, ex. incendiul, explozia ca urmare a producerii unui
         cutremur ori alunecări de teren;`,
-        ` ⁃ pentru pagubele indirecte provocate locuinței, consecința directă a
+        `pentru pagubele indirecte provocate locuinței, consecința directă a
         producerii evenimentului asigurat, dacă daunele sunt confirmate de
         autoritățile competente.`,
-        `✔︎ În funcție de tipul locuinței, suma asigurată e reprezentată de
+        `În funcție de tipul locuinței, suma asigurată e reprezentată de
         echivalentul în lei al sumei de 20.000 EUR pentru locuințele „Tip A”,
         respectiv 10.000 EUR pentru cele „Tip B”, calculată la cursul Băncii
         Naționale a României valabil la data încheierii asigurării.`,
@@ -131,21 +132,22 @@ export class InfoDocComponent implements OnInit {
         text: 'Ce nu se asigură?',
         classes: '',
       },
+      listClass: 'red',
       textContents: [
-        `✖︎ Daunele provocate bunurilor de orice fel, altele decât locuința;`,
-        `✖︎ Daunele provocate de inundații produse în timpul formării unor
+        `Daunele provocate bunurilor de orice fel, altele decât locuința;`,
+        `Daunele provocate de inundații produse în timpul formării unor
         lacuri de acumulare sau de schimbarea artificială a cursurilor de
         apă;`,
-        ` ✖︎ Daunele provocate de evenimentele asigurate, dacă aceste fenomene
+        `Daunele provocate de evenimentele asigurate, dacă aceste fenomene
         au fost prilejuite, înlesnite ori agravate de săpături sau lucrări
         edilitare de orice fel, lucrări de prospecțiuni, explorări ori
         exploatări miniere sau petroliere, la suprafață ori în profunzime;`,
-        ` ✖︎ Daunele provocate de tasarea (lăsarea) terenului de fundație;`,
-        `✖︎ Daunele provocate de formarea de crăpături în terenul de fundație
+        `Daunele provocate de tasarea (lăsarea) terenului de fundație;`,
+        `Daunele provocate de formarea de crăpături în terenul de fundație
         sau în terenul din preajma clădirii, datorită variației de volum a
         terenului, ca urmare a contracției/dilatării produse de
         îngheț/dezgheț;`,
-        `✖︎ Daunele provocate terenului care împrejmuiește locuința și care
+        `Daunele provocate terenului care împrejmuiește locuința și care
         nu este asociat noțiunii de locuință și nu face obiectul asigurării.`,
       ],
     },
@@ -158,15 +160,16 @@ export class InfoDocComponent implements OnInit {
         text: 'Există restricții de acoperire?',
         classes: '',
       },
+      listClass: 'danger',
       textContents: [
-        `! Anexele, dependințele, dotările și utilitățile nelegate structural
+        `Anexele, dependințele, dotările și utilitățile nelegate structural
         de locuință, anexele ce nu folosesc în mod direct scopului locativ
         (garaje, magazii, șoproane, grajduri, jardiniere, pergole,
         împrejmuiri, pătule, depozite etc.), sau instalațiile și amenajările
         speciale (piscine, saune, instalații solare sau eoliene, rampe auto
         etc.);`,
-        `! Locuințele situate în zone cu interdicție de construire;`,
-        `! Locuințele încadrate în clasa I de risc seismic.`,
+        `Locuințele situate în zone cu interdicție de construire;`,
+        `Locuințele încadrate în clasa I de risc seismic.`,
       ],
     },
     {
@@ -178,11 +181,10 @@ export class InfoDocComponent implements OnInit {
         text: 'Unde beneficiez de asigurare?',
         classes: 'px-8',
       },
+      listClass: 'blue',
       textContents: [
-        `✔︎ Obiectul contractului de asigurare îl reprezintă construcțiile cu
+        `Obiectul contractului de asigurare îl reprezintă construcțiile cu
         destinația de locuință situate pe teritoriul României.`,
-        `! Locuințele situate în zone cu interdicție de construire;`,
-        `! Locuințele încadrate în clasa I de risc seismic.`,
       ],
     },
     {
@@ -194,18 +196,19 @@ export class InfoDocComponent implements OnInit {
         text: 'Ce obligații am?',
         classes: '',
       },
+      listClass: 'green-2',
       textContents: [
-        `• 	Să încheiați câte un PAD pentru fiecare locuință deținută în proprietate și, la expirarea valabilității acestora, sa le reînnoiți;`,
-        `• 	Să predați, în cazul înstrăinării locuinței, PAD dobânditorului, noul proprietar având obligația de a notifica achiziția în 5 zile asigurătorului care a eliberat PAD;`,
-        `• 	Să întrețineți corespunzător locuința asigurată, în scopul prevenirii producerii ori apariției oricărui eveniment asigurat;`,
-        `• 	Să răspundeți în scris la solicitările PAID cu privire la împrejurările producerii evenimentului asigurat și să vă conformați recomandărilor PAID;`,
-        `• 	Să înștiințați imediat producerea evenimentului asigurat organelor abilitate potrivit legii (unitățile de pompieri, poliție, primăria localității etc.), cerând întocmirea de acte cu privire la cauzele și împrejurările producerii sau apariției evenimentului, daunele provocate, precum și precizarea vinovaților de eventuala mărire a pagubei și să le prezentați PAID;`,
-        `• 	Să înștiințați imediat în scris PAID ori asigurătorul care a eliberat PAD despre producerea riscului asigurat, precizând seria, numărul și data emiterii poliței, daunele suferite și mărimea probabilă a daunei;`,
-        `• 	Să faceți dovada dreptului de proprietate sau de administrare asupra locuinței asigurate.`,
+        `Să încheiați câte un PAD pentru fiecare locuință deținută în proprietate și, la expirarea valabilității acestora, sa le reînnoiți;`,
+        `Să predați, în cazul înstrăinării locuinței, PAD dobânditorului, noul proprietar având obligația de a notifica achiziția în 5 zile asigurătorului care a eliberat PAD;`,
+        `Să întrețineți corespunzător locuința asigurată, în scopul prevenirii producerii ori apariției oricărui eveniment asigurat;`,
+        `Să răspundeți în scris la solicitările PAID cu privire la împrejurările producerii evenimentului asigurat și să vă conformați recomandărilor PAID;`,
+        `Să înștiințați imediat producerea evenimentului asigurat organelor abilitate potrivit legii (unitățile de pompieri, poliție, primăria localității etc.), cerând întocmirea de acte cu privire la cauzele și împrejurările producerii sau apariției evenimentului, daunele provocate, precum și precizarea vinovaților de eventuala mărire a pagubei și să le prezentați PAID;`,
+        `Să înștiințați imediat în scris PAID ori asigurătorul care a eliberat PAD despre producerea riscului asigurat, precizând seria, numărul și data emiterii poliței, daunele suferite și mărimea probabilă a daunei;`,
+        `Să faceți dovada dreptului de proprietate sau de administrare asupra locuinței asigurate.`,
       ],
     },
     {
-      classes: 'bg-light-green-2',
+      classes: 'bg-yellow',
       icon: {
         name: 'md-coins',
       },
@@ -213,6 +216,7 @@ export class InfoDocComponent implements OnInit {
         text: 'Când și cum plătesc?',
         classes: '',
       },
+      listClass: 'yellow',
       textContents: [
         `Plata primei de asigurare se face integral și anticipat, în numerar sau prin virament în contul asigurătorului care eliberează PAD.`,
         `Primele de asigurare reprezintă echivalentul în lei, la cursul Băncii Naționale a României valabil la data efectuării plății, a 20 EUR pentru locuințele de „Tip A”, respectiv 10 EUR pentru locuințele de „Tip B”.`,
@@ -227,6 +231,7 @@ export class InfoDocComponent implements OnInit {
         text: 'Când începe și când încetează acoperirea?',
         classes: '',
       },
+      listClass: 'light-blue',
       textContents: [
         `În cazul polițelor noi, asigurarea începe din a cincea zi de la data plății primei și încheierii contractului de asigurare.`,
         `În cazul polițelor care se reînnoiesc, asigurarea începe din ziua următoare celei în care s-a plătit prima și încheiat contractul de asigurare, dar nu mai devreme de ora 0,00 a zilei următoare celei în care încetează vechiul contract.`,
@@ -242,6 +247,7 @@ export class InfoDocComponent implements OnInit {
         text: 'Cum pot sa reziliez contractul?',
         classes: '',
       },
+      listClass: 'dark',
       textContents: [
         `PAD nu poate fi reziliată. Chiar și în cazul înstrăinării locuinței, PAD rămâne în vigoare pe întreaga perioadă de valabilitate. `,
       ],
