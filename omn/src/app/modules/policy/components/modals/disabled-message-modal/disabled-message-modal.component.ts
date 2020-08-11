775732c4d3ee3ee9a621ccd77cc236534d386ef8
Samson Nwokike <samson.ikechukwu@softescu.com>
2020-08-11 07:44:58 +0100

    set up disabled meassage modal

---
+++ omn/src/app/modules/policy/components/modals/disabled-message-modal/disabled-message-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-disabled-message-modal',
+  templateUrl: './disabled-message-modal.component.html',
+  styleUrls: ['./disabled-message-modal.component.scss'],
+})
+export class DisabledMessageModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- omn/src/app/modules/policy/components/policy-card-list/policy-card-list.component.ts
+++ omn/src/app/modules/policy/components/policy-card-list/policy-card-list.component.ts
@@ -5,6 +5,8 @@ import {
   ChangeDetectionStrategy,
   ViewChild,
 } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { DisabledMessageModalComponent } from '../modals/disabled-message-modal/disabled-message-modal.component';
 import { ImageCard } from 'src/app/shared/models/component/image-card';
 import { IonTextItem } from 'src/app/shared/models/component/ion-text-item';
 
@@ -18,7 +20,16 @@ export class PolicyCardListComponent implements OnInit {
   @Input()
   items: Array<ImageCard> = [];
   @Input() title: IonTextItem = null;
-  constructor() {}
+  constructor(public modalController: ModalController) {}
 
   ngOnInit() {}
+
+  async presentModal(index) {
+    console.log(this.items[index]);
+    const modal = await this.modalController.create({
+      component: DisabledMessageModalComponent,
+      cssClass: 'my-custom-modal-class',
+    });
+    return await modal.present();
+  }
 }
