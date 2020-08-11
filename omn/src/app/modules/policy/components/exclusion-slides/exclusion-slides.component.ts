572ae2d704bcff274e80aaec773e97113d4fed09
maxciceu <maxciceu2@gmail.com>
2020-08-11 11:26:00 +0300

    Update UI

---
+++ omn/src/app/modules/policy/components/exclusion-slides/exclusion-slides.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exclusion-slides',
+  templateUrl: './exclusion-slides.component.html',
+  styleUrls: ['./exclusion-slides.component.scss'],
+})
+export class ExclusionSlidesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
