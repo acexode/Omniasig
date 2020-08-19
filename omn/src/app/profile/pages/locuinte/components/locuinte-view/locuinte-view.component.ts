7e747de6fc8bf584601f5f14fe86b2318da62361
Samson Nwokike <samson.ikechukwu@softescu.com>
2020-08-19 04:44:43 +0100

    locuinta form step setup

--- omn/src/app/profile/pages/locuinte/components/locuinte-view/locuinte-view.component.ts
+++ omn/src/app/profile/pages/locuinte/components/locuinte-view/locuinte-view.component.ts
@@ -10,12 +10,15 @@ import { BehaviorSubject, combineLatest } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { CustomRouterService } from 'src/app/core/services/custom-router/custom-router.service';
 import { subPageHeaderDefault } from 'src/app/shared/data/sub-page-header-default';
+import { subPageHeaderPrimary } from 'src/app/shared/data/sub-page-header-primary';
 import {
   Locuinte,
   LocuintaState,
 } from 'src/app/shared/models/data/locuinte.interface';
 import { LocuinteService } from '../../services/locuinte/locuinte.service';
 import { LocuinteFormType } from 'src/app/shared/models/modes/locuinte-form-modes';
+import { FormGroup } from '@angular/forms';
+import { LocuinteFormService } from '../../services/locuinte-form/locuinte-form.service';
 
 @Component({
   selector: 'app-locuinte-view',
@@ -26,16 +29,43 @@ export class LocuinteViewComponent implements OnInit {
   headerConfig = null;
   locuinta$: BehaviorSubject<Locuinte> = new BehaviorSubject(null);
   variant = 'not-insured'; // not-insured, not-found, found.
+  dataModel: Locuinte;
   formMode: LocuintaState;
   locuintaState = LocuintaState;
-  formType: LocuinteFormType.ADDRESS;
+  formType: LocuinteFormType;
+  locuinteFormType = LocuinteFormType;
+  formConfigs: {
+    address: any;
+    place: any;
+  } = {
+    address: {},
+    place: {},
+  };
+  formGroups: {
+    address: FormGroup;
+    place: FormGroup;
+  } = {
+    address: null,
+    place: null,
+  };
+  formData: {
+    address: any;
+    place: any;
+  } = {
+    address: {},
+    place: {},
+  };
+  formInstance: { group: FormGroup; config: any; data: any } = null;
+  buttonVisible: boolean;
+  buttonText: string;
   @HostBinding('class') color = 'ion-color-white-page';
   constructor(
     private routerS: CustomRouterService,
     private aRoute: ActivatedRoute,
     private cdRef: ChangeDetectorRef,
     private navCtrl: NavController,
-    private locuinteS: LocuinteService
+    private locuinteS: LocuinteService,
+    private formS: LocuinteFormService
   ) {}
 
   ngOnInit() {
@@ -57,6 +87,9 @@ export class LocuinteViewComponent implements OnInit {
           this.locuinteS.getSingleLocuinta(id).subscribe((val: Locuinte) => {
             if (val) {
               this.locuinta$.next(val);
+              this.dataModel = val;
+              this.buildFormAdd();
+              this.initForm();
               this.cdRef.markForCheck();
             } else {
               this.navCtrl.navigateRoot(['/profil', 'locuinte']);
@@ -97,7 +130,48 @@ export class LocuinteViewComponent implements OnInit {
     }
   }
 
+  initForm() {
+    switch (this.formMode) {
+      case this.locuintaState.INVALID:
+      case this.locuintaState.INCOMPLETE:
+        if (!this.formInstance) {
+          this.formInstance = {
+            config: this.formConfigs.address,
+            group: this.formGroups.address,
+            data: this.formData.address,
+          };
+        }
+        this.formType = LocuinteFormType.ADDRESS;
+        break;
+    }
+  }
+
+  buildFormAdd() {
+    this.formConfigs.address = this.formS.buildFormConfig(
+      LocuinteFormType.ADDRESS
+    );
+    this.formConfigs.place = this.formS.buildFormConfig(LocuinteFormType.PLACE);
+    this.formData.address = this.formS.getFormFieldsData(
+      this.formConfigs.address
+    );
+    this.formData.place = this.formS.getFormFieldsData(this.formConfigs.place);
+    this.formGroups.address = this.formS.buildAddressSubform(this.dataModel);
+    this.formGroups.place = this.formS.buildLocuinteSubform(this.dataModel);
+  }
+
   formCustomEvents() {}
 
   handleFormSubmit() {}
+
+  nextStep() {
+    this.headerConfig = subPageHeaderPrimary('Informații  locuință');
+    this.buttonVisible = true;
+    this.buttonText = 'Salvează';
+    this.formInstance = {
+      config: this.formConfigs.place,
+      group: this.formGroups.place,
+      data: this.formData.place,
+    };
+    this.formType = LocuinteFormType.PLACE;
+  }
 }
