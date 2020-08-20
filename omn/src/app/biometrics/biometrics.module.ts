38844bbd516fe239d9543e68a824c9663b3e6ca4
Okeke Chekwube <okekechekwube1@gmail.com>
2020-08-20 20:03:53 +0100

    Created the components, routes, templates

---
+++ omn/src/app/biometrics/biometrics.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BiometricsRoutingModule } from './biometrics-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BiometricsRoutingModule
+  ]
+})
+export class BiometricsModule { }
---
+++ omn/src/app/biometrics/more-details/more-details.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MoreDetailsRoutingModule } from './more-details-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MoreDetailsRoutingModule
+  ]
+})
+export class MoreDetailsModule { }
