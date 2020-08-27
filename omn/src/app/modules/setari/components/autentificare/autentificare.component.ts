2d017faf10c6237d5b2a1c84100d746631b8a046
maxciceu <maximilian.ciceu@softescu.com>
2020-08-27 23:20:08 +0300

    Update Fonts

--- omn/src/app/modules/setari/components/autentificare/autentificare.component.ts
+++ omn/src/app/modules/setari/components/autentificare/autentificare.component.ts
@@ -1,20 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { SubPageHeader } from 'src/app/shared/models/component/sub-page-header';
+import { Component, HostBinding, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { unsubscriberHelper } from 'src/app/core/helpers/unsubscriber.helper';
 import { subPageHeaderDefault } from 'src/app/shared/data/sub-page-header-default';
+import { SubPageHeader } from 'src/app/shared/models/component/sub-page-header';
 
 @Component({
   selector: 'app-autentificare',
   templateUrl: './autentificare.component.html',
   styleUrls: ['./autentificare.component.scss'],
 })
-export class AutentificareComponent implements OnInit {
+export class AutentificareComponent implements OnInit, OnDestroy {
+  @HostBinding('class') color = 'ion-color-white-page';
   headerConfig: SubPageHeader = {
     ...subPageHeaderDefault('Autentificare'),
     trailingIcon: null,
   };
 
-  constructor() { }
+  formGroup = this.fb.group({
+    faceId: this.fb.control(false),
+  });
+  faceIdS: Subscription;
 
-  ngOnInit() {}
+  constructor(private fb: FormBuilder) {}
+  ngOnInit() {
+    this.handleSubmission();
+  }
 
+  // Try to attach this after data loading for the toggles.
+  handleSubmission() {
+    unsubscriberHelper(this.faceIdS);
+    this.faceIdS = this.formGroup.get('faceId').valueChanges.subscribe((v) => {
+      // TODO: implement data push.
+    });
+  }
+
+  ngOnDestroy() {
+    unsubscriberHelper(this.faceIdS);
+  }
 }
