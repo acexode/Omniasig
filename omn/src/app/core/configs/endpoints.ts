95fc8a3f0699335f27b328aee8450877375cc75b
Ereyomi Oluwaseyi Samuel <ereyomi.oluwaseyil@softescu.com>
2020-08-31 18:38:01 +0100

    refactor

--- omn/src/app/core/configs/endpoints.ts
+++ omn/src/app/core/configs/endpoints.ts
@@ -2,35 +2,34 @@ import { environment } from '../../../environments/environment';
 
 export const serverBaseUrl = environment.serverUrl;
 export const authEndpoints = {
-  login: serverBaseUrl + '/User/LoginAndCreateToken',
-  findUserByPhoneNumber:
-    serverBaseUrl + '/UserProfile/GetUserNameByPhoneNumber',
-  getUserProfile: serverBaseUrl + '/UserProfile/GetUserProfile',
+    login: serverBaseUrl + '/User/LoginAndCreateToken',
+    findUserByPhoneNumber:
+        serverBaseUrl + '/UserProfile/GetUserNameByPhoneNumber',
+    getUserProfile: serverBaseUrl + '/UserProfile/GetUserProfile',
 };
 
 export const baseEndpoints = {
-  policy: serverBaseUrl + '/policy',
-  userProfile: serverBaseUrl + '/UserProfile',
+    policy: serverBaseUrl + '/policy',
+    userProfile: serverBaseUrl + '/UserProfile',
 };
 
 export const policyEndpoints = {
-  base: baseEndpoints.policy,
-  userPoliciesBase: baseEndpoints.policy,
-  userPoliciesArchive: baseEndpoints.policy + '/archive',
-  userOffersBase: baseEndpoints.policy + '/offers',
+    base: baseEndpoints.policy,
+    userPoliciesBase: baseEndpoints.policy,
+    userPoliciesArchive: baseEndpoints.policy + '/archive',
+    userOffersBase: baseEndpoints.policy + '/offers',
 };
 
 export const locuinteEndpoints = {
-  base: serverBaseUrl + '/locuinte',
-  add: serverBaseUrl + '/Address/AddAddress',
-  makeHomeAddress: serverBaseUrl + '/Address/MakeHomeAddress',
-  AlluserLocation: serverBaseUrl + '/Address/GetAllAddressesForLoggedUser',
-  updateAddress: serverBaseUrl + '/Location/UpdateLocationForAddressId',
-  disAbleLocation: serverBaseUrl + '/Location/DisableLocationForAddressId',
+    base: serverBaseUrl + '/locuinte',
+    add: serverBaseUrl + '/Address/AddAddress',
+    makeHomeAddress: serverBaseUrl + '/Address/MakeHomeAddress',
+    AlluserLocation: serverBaseUrl + '/Address/GetAllAddressesForLoggedUser',
+    updateAddress: serverBaseUrl + '/Location/UpdateLocationForAddressId',
+    disAbleLocation: serverBaseUrl + '/Location/DisableLocationForAddressId',
 };
 
 export const phoneNumberEndPoints = {
-  RequestNewPhoneNumberChange:
-    baseEndpoints.userProfile + '/RequestNewPhoneNumberChange',
-  ConfirmNewPhoneNumber: baseEndpoints.userProfile + '/ConfirmNewPhoneNumber',
+    RequestNewPhoneNumberChange: baseEndpoints.userProfile + '/RequestNewPhoneNumberChange',
+    ConfirmNewPhoneNumber: baseEndpoints.userProfile + '/ConfirmNewPhoneNumber',
 };
