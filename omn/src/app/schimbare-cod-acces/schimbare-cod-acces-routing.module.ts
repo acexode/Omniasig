f4c6ace92901c4ca18733ffde848db333e4b47e2
maxciceu <maximilian.ciceu@softescu.com>
2020-09-01 09:38:41 +0300

    Update menu link and fix component location

--- omn/src/app/schimbare-cod-acces/schimbare-cod-acces-routing.module.ts
+++ omn/src/app/schimbare-cod-acces/schimbare-cod-acces-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CodActualComponent } from './cod-actual/cod-actual.component';
+import { ChangeSuccessfulComponent } from './change-successful/change-successful.component';
 import { CodAccesNouComponent } from './cod-acces-nou/cod-acces-nou.component';
+import { CodActualComponent } from './cod-actual/cod-actual.component';
 import { ConfirmareCodAccesComponent } from './confirmare-cod-acces/confirmare-cod-acces.component';
-import { ChangeSuccessfulComponent } from '../schimbare-numar-telefon/change-successful/change-successful.component';
 
 const routes: Routes = [
   {
--- omn/src/app/schimbare-cod-acces/schimbare-cod-acces.module.ts
+++ omn/src/app/schimbare-cod-acces/schimbare-cod-acces.module.ts
@@ -4,11 +4,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { OmnInputsModule } from '../shared/modules/omn-inputs/omn-inputs.module';
 import { SharedModule } from '../shared/shared.module';
-import { SchimbareCodAccesPageRoutingModule } from './schimbare-cod-acces-routing.module';
-import { CodActualComponent } from './cod-actual/cod-actual.component';
+import { ChangeSuccessfulComponent } from './change-successful/change-successful.component';
 import { CodAccesNouComponent } from './cod-acces-nou/cod-acces-nou.component';
+import { CodActualComponent } from './cod-actual/cod-actual.component';
 import { ConfirmareCodAccesComponent } from './confirmare-cod-acces/confirmare-cod-acces.component';
-import { ChangeSuccessfulComponent } from '../schimbare-numar-telefon/change-successful/change-successful.component';
+import { SchimbareCodAccesPageRoutingModule } from './schimbare-cod-acces-routing.module';
 
 @NgModule({
   imports: [
