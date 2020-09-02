77351784409c990c4fc588b3e0d1e004e2aa09c8
maxciceu <maximilian.ciceu@softescu.com>
2020-09-02 11:56:55 +0300

    Tweak some colors

--- omn/src/app/schimbare-numar-telefon/change-phone-number/change-phone-number.component.ts
+++ omn/src/app/schimbare-numar-telefon/change-phone-number/change-phone-number.component.ts
@@ -1,109 +1,106 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, HostBinding, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 import { subPageHeaderDefault } from 'src/app/shared/data/sub-page-header-default';
 import { IonInputConfig } from 'src/app/shared/models/component/ion-input-config';
 import { IonTextItem } from 'src/app/shared/models/component/ion-text-item';
-import { PhonenumberService } from '../services/phonenumber.service';
 import { RequestNewPhoneNumberChange } from '../models/RequestNewPhoneNumberChange.interface';
-import { AuthService } from 'src/app/core/services/auth/auth.service';
-import { Subscription } from 'rxjs';
+import { PhonenumberService } from '../services/phonenumber.service';
 
-@Component( {
-    selector: 'app-change-phone-number',
-    templateUrl: './change-phone-number.component.html',
-    styleUrls: [ './change-phone-number.component.scss' ],
-} )
+@Component({
+  selector: 'app-change-phone-number',
+  templateUrl: './change-phone-number.component.html',
+  styleUrls: ['./change-phone-number.component.scss'],
+})
 export class ChangePhoneNumberComponent implements OnInit, OnDestroy {
-    headerConfig = subPageHeaderDefault( 'Schimbare număr  telefon' );
-    label: IonTextItem = {
-        text: 'Introdu noul număr de telefon',
-        classes: 'link-small color-tertiary-grey w-100 bg-white pb-8',
-        slot: 'end',
-    };
-    config: IonInputConfig = {
-        placeholder: '07XX XXX XXX',
-        type: 'tel',
-        inputMode: 'tel',
-        size: 100,
-        inputLabel: this.label,
-        clearable: true,
-        minLength: 10,
-        maxLength: 11,
-    };
-    teleForm: FormGroup;
+  @HostBinding('class') color = 'ion-color-white-page';
+  headerConfig = subPageHeaderDefault('Schimbare număr  telefon');
+  label: IonTextItem = {
+    text: 'Introdu noul număr de telefon',
+    classes: 'w-100 pb-8',
+    slot: 'end',
+  };
+  config: IonInputConfig = {
+    placeholder: '07XX XXX XXX',
+    type: 'tel',
+    inputMode: 'tel',
+    size: 100,
+    inputLabel: this.label,
+    clearable: true,
+    minLength: 10,
+    maxLength: 11,
+  };
+  teleForm: FormGroup;
 
-    sub: Subscription;
-    error = false;
+  sub: Subscription;
+  error = false;
 
-    constructor(
-        private router: Router,
-        private formBuilder: FormBuilder,
-        private phS: PhonenumberService,
-        private authS: AuthService,
-    ) { }
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private phS: PhonenumberService,
+    private authS: AuthService
+  ) {}
 
-    ngOnInit() {
-        this.initForm();
-    }
+  ngOnInit() {
+    this.initForm();
+  }
 
-    initForm() {
-        this.teleForm = this.formBuilder.group( {
-            phoneNumber: [
-                '',
-                [
-                    Validators.required,
-                    Validators.pattern( /^07[0-9].*$/ ),
-                    Validators.minLength( 9 ),
-                ],
-            ],
-        } );
+  initForm() {
+    this.teleForm = this.formBuilder.group({
+      phoneNumber: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^07[0-9].*$/),
+          Validators.minLength(9),
+        ],
+      ],
+    });
 
-        this.sub = this.teleForm.valueChanges.subscribe( ( value ) => {
-            this.onUserInput( value.phoneNumber );
-        } );
-    }
+    this.sub = this.teleForm.valueChanges.subscribe((value) => {
+      this.onUserInput(value.phoneNumber);
+    });
+  }
 
-    onUserInput( phoneNumber: number ) {
-        if (phoneNumber){
-            if ( phoneNumber.toString().length > 0 ) {
-                this.error = false;
-            } else {
-                this.error = true;
-            }
-        }
-    }
-
-    proceed() {
-        const newPhoneNumber = this.teleForm.controls.phoneNumber.value;
-
-        this.authS.getAuthState().subscribe( authData => {
-            const { userId } = authData.account;
-            const requestNewPhoneDetails: RequestNewPhoneNumberChange = {
-                userNameOrId: userId,
-                newPhoneNumber,
-            };
-            this.phS.updatePhoneNumber( requestNewPhoneDetails )
-                .subscribe(
-                    response => {
-                        this.router.navigate( [
-                            'phone-number/confirm-number',
-                            newPhoneNumber,
-                        ] );
-                    },
-                    err => {
-                        this.isError();
-                    }
-                );
-        } );
-    }
-
-    isError() {
-        this.teleForm.reset();
+  onUserInput(phoneNumber: number) {
+    if (phoneNumber) {
+      if (phoneNumber.toString().length > 0) {
+        this.error = false;
+      } else {
         this.error = true;
+      }
     }
+  }
 
-    ngOnDestroy() {
-        this.sub.unsubscribe();
-    }
+  proceed() {
+    const newPhoneNumber = this.teleForm.controls.phoneNumber.value;
+
+    this.authS.getAuthState().subscribe((authData) => {
+      const { userId } = authData.account;
+      const requestNewPhoneDetails: RequestNewPhoneNumberChange = {
+        userNameOrId: userId,
+        newPhoneNumber,
+      };
+      this.phS.updatePhoneNumber(requestNewPhoneDetails).subscribe(
+        (response) => {
+          this.router.navigate(['phone-number/confirm-number', newPhoneNumber]);
+        },
+        (err) => {
+          this.isError();
+        }
+      );
+    });
+  }
+
+  isError() {
+    this.teleForm.reset();
+    this.error = true;
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
--- omn/src/app/schimbare-numar-telefon/verify-phone-number/verify-phone-number.component.ts
+++ omn/src/app/schimbare-numar-telefon/verify-phone-number/verify-phone-number.component.ts
@@ -1,145 +1,134 @@
 import { Location } from '@angular/common';
 import {
-    Component,
-    OnInit,
-    ViewChild,
-    OnDestroy,
-    AfterViewInit,
+  AfterViewInit,
+  Component,
+  HostBinding,
+  OnDestroy,
+  OnInit,
+  ViewChild,
 } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IonInput } from '@ionic/angular';
 import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 import { CustomTimersService } from 'src/app/core/services/custom-timers/custom-timers.service';
 import { subPageHeaderDefault } from 'src/app/shared/data/sub-page-header-default';
 import { IonInputConfig } from 'src/app/shared/models/component/ion-input-config';
-import { AuthService } from 'src/app/core/services/auth/auth.service';
-import { PhonenumberService } from '../services/phonenumber.service';
 import { ConfirmNewPhoneNumber } from '../models/ConfirmNewPhoneNumber.interface';
+import { PhonenumberService } from '../services/phonenumber.service';
 
-@Component( {
-    selector: 'app-verify-phone-number',
-    templateUrl: './verify-phone-number.component.html',
-    styleUrls: [ './verify-phone-number.component.scss' ],
-} )
+@Component({
+  selector: 'app-verify-phone-number',
+  templateUrl: './verify-phone-number.component.html',
+  styleUrls: ['./verify-phone-number.component.scss'],
+})
 export class VerifyPhoneNumberComponent
-    implements OnInit, OnDestroy, AfterViewInit {
-    headerConfig = subPageHeaderDefault( 'Cod de verificare' );
-    min = '00';
-    sec: any = 59;
-    digitsLength = 0;
-    @ViewChild( 'inputField' ) inputField: IonInput;
-    sub: Subscription;
-    phoneNumber = null;
+  implements OnInit, OnDestroy, AfterViewInit {
+  @HostBinding('class') color = 'ion-color-white-page';
+  headerConfig = subPageHeaderDefault('Cod de verificare');
+  min = '00';
+  sec: any = 59;
+  digitsLength = 0;
+  @ViewChild('inputField') inputField: IonInput;
+  sub: Subscription;
+  phoneNumber = null;
 
-    config: IonInputConfig = {
-        type: 'number',
-        inputMode: 'number',
-    };
-    passForm: FormGroup;
-    InvalidCode = false;
-    constructor(
-        private route: ActivatedRoute,
-        private router: Router,
-        private timers: CustomTimersService,
-        private formBuilder: FormBuilder,
-        private location: Location,
-        private phS: PhonenumberService,
-        private authS: AuthService,
-    ) {
-        this.route.params.subscribe( ( params ) => {
-            if ( params.phone ) {
-                this.phoneNumber = params.phone;
-            } else {
-                this.location.back();
-            }
-        } );
+  config: IonInputConfig = {
+    type: 'number',
+    inputMode: 'number',
+  };
+  passForm: FormGroup;
+  InvalidCode = false;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private timers: CustomTimersService,
+    private formBuilder: FormBuilder,
+    private location: Location,
+    private phS: PhonenumberService,
+    private authS: AuthService
+  ) {
+    this.route.params.subscribe((params) => {
+      if (params.phone) {
+        this.phoneNumber = params.phone;
+      } else {
+        this.location.back();
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.initForm();
+  }
+  initForm() {
+    this.passForm = this.formBuilder.group({
+      digit: [
+        '',
+        [Validators.required, Validators.minLength(6), Validators.maxLength(6)],
+      ],
+    });
+
+    this.sub = this.passForm.valueChanges.subscribe((value) => {
+      this.changeInput(value.digit);
+    });
+  }
+
+  changeInput(digit: number) {
+    this.InvalidCode = false;
+    if (digit) {
+      this.digitsLength = digit.toString().length;
+    } else {
+      this.digitsLength = 0;
     }
+  }
 
-    ngOnInit() {
-        this.initForm();
-    }
-    initForm() {
-        this.passForm = this.formBuilder.group( {
-            digit: [
-                '',
-                [ Validators.required, Validators.minLength( 6 ), Validators.maxLength( 6 ) ],
-            ],
-        } );
+  ngAfterViewInit() {
+    this.startTimer();
+  }
 
-        this.sub = this.passForm.valueChanges.subscribe( ( value ) => {
-            this.changeInput( value.digit );
-        } );
-    }
+  startTimer() {
+    this.timers.buildTimer(59).subscribe((time: number) => {
+      this.sec = time;
+    });
+  }
 
-    changeInput( digit: number ) {
-        this.InvalidCode = false;
-        if ( digit ) {
-            this.digitsLength = digit.toString().length;
-        } else {
-            this.digitsLength = 0;
+  resendSMS() {
+    this.startTimer();
+  }
+
+  proceed() {
+    this.router.navigate(['phone-number/change-successful']);
+  }
+
+  continue() {
+    this.authS.getAuthState().subscribe((authData) => {
+      const { userId } = authData.account;
+      const requestNewPhoneDetails: ConfirmNewPhoneNumber = {
+        confirmationCode: this.passForm.controls.digit.value,
+        userNameOrId: userId,
+        newPhoneNumber: this.phoneNumber,
+      };
+      this.phS.validatePhoneCode(requestNewPhoneDetails).subscribe(
+        (response) => {
+          this.proceed();
+        },
+        (err) => {
+          this.passForm.reset();
+          this.InvalidCode = true;
         }
-    }
+      );
+    });
+  }
 
-    continueTest() {
-        if ( this.passForm.controls.digit.value === 123456 ) {
-            this.proceed();
-        } else {
-            this.InvalidCode = true;
-            setTimeout( () => {
-                this.InvalidCode = false;
-                this.passForm.reset();
-            }, 2000 );
-        }
-    }
+  spawnInput() {
+    this.inputField.getInputElement().then((input) => {
+      input.focus();
+      input.click();
+    });
+  }
 
-    ngAfterViewInit() {
-        this.startTimer();
-    }
-
-    startTimer() {
-        this.timers.buildTimer( 59 ).subscribe( ( time: number ) => {
-            this.sec = time;
-        } );
-    }
-
-    resendSMS() {
-        this.startTimer();
-    }
-
-    proceed() {
-        this.router.navigate( [ 'phone-number/change-successful' ] );
-    }
-
-    continue() {
-        this.authS.getAuthState().subscribe( authData => {
-            const { userId } = authData.account;
-            const requestNewPhoneDetails: ConfirmNewPhoneNumber = {
-                confirmationCode: this.passForm.controls.digit.value,
-                userNameOrId: userId,
-                newPhoneNumber: this.phoneNumber,
-            };
-            this.phS.validatePhoneCode( requestNewPhoneDetails )
-                .subscribe(
-                    response => {
-                        this.proceed();
-                    },
-                    err => {
-                        this.InvalidCode = true;
-                        this.passForm.reset();
-                    }
-                );
-        } );
-    }
-
-    spawnInput() {
-        this.inputField.getInputElement().then( ( input ) => {
-            input.focus();
-            input.click();
-        } );
-    }
-
-    ngOnDestroy() {
-        this.sub.unsubscribe();
-    }
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
