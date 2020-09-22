a7cb4a974b47dfe9a77fc84f206ff680d2cc9d79
maxciceu <maximilian.ciceu@softescu.com>
2020-09-22 17:00:31 +0300

    fix bug

--- omn/src/app/core/services/auth/auth.service.ts
+++ omn/src/app/core/services/auth/auth.service.ts
@@ -141,8 +141,9 @@ export class AuthService {
   }
 
   accountActivated(acc: Account) {
-    return acc.isBiometricValid === true && acc.isEmailConfirmed === true
-      ? true
+    return acc
+      ? get(acc, 'isBiometricValid', false) === true &&
+          get(acc, 'isEmailConfirmed', false) === true
       : false;
   }
 
