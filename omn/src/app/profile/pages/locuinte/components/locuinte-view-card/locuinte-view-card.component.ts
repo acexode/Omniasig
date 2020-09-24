b320924867096dae1cc220ad370647b2e9701cbc
Ereyomi Oluwaseyi Samuel <ereyomi.oluwaseyil@softescu.com>
2020-09-24 15:24:43 +0100

    refactor: locuite view

--- omn/src/app/profile/pages/locuinte/components/locuinte-view-card/locuinte-view-card.component.ts
+++ omn/src/app/profile/pages/locuinte/components/locuinte-view-card/locuinte-view-card.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { AmplusService } from 'src/app/modules/policy/services/amplus.service';
+import { PolicyDataService } from 'src/app/modules/policy/services/policy-data.service';
 import { Locuinte } from 'src/app/shared/models/data/locuinte.interface';
 import { PriceModalComponent } from '../../../components/modals/price-modal/price-modal.component';
 
-@Component({
+@Component( {
   selector: 'app-locuinte-view-card',
   templateUrl: './locuinte-view-card.component.html',
-  styleUrls: ['./locuinte-view-card.component.scss'],
-})
+  styleUrls: [ './locuinte-view-card.component.scss' ],
+} )
 export class LocuinteViewCardComponent implements OnInit {
   @Input() variant: string;
   @Input() locuintaData: Locuinte;
-  constructor(public modalController: ModalController, private ampS: AmplusService) {}
+  constructor(
+    public modalController: ModalController,
+    private ampS: AmplusService,
+    private policyS: PolicyDataService
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
-  getAmplus(){
-    const amplusAddressId = 79;
+  getAmplus() {
+    // generateOffer
     const generateOffer = false;
     // payload
     const payload = {
@@ -32,21 +38,21 @@ export class LocuinteViewCardComponent implements OnInit {
       propertyCessionList: null
     };
 
-    this.ampS.CreateAmplusInsuranceOffer( amplusAddressId, generateOffer, payload )
+    this.ampS.CreateAmplusInsuranceOffer( this.locuintaData.id, generateOffer, payload )
       .subscribe( d => {
-        this.presentModal( d.response?.ofertaResponse?.prima);
+        this.presentModal( d.response?.ofertaResponse?.prima );
       } );
   }
 
   async presentModal( data = 0 ) {
-    const modal = await this.modalController.create({
+    const modal = await this.modalController.create( {
       component: PriceModalComponent,
       cssClass: 'disabled-message-modal-class',
       componentProps: {
         variant: this.variant,
         prima: data ? data : 0,
       }
-    });
+    } );
     return await modal.present();
   }
 }
