15721cdcaba2301c612d77f6766dda0ddeeb8c64
Ereyomi Oluwaseyi Samuel <ereyomi.oluwaseyil@softescu.com>
2020-09-30 12:30:07 +0100

    feat:autoCapitalize added to date-personale registeration component

--- omn/src/app/registration/date-personale/date-personale.component.ts
+++ omn/src/app/registration/date-personale/date-personale.component.ts
@@ -31,6 +31,7 @@ export class DatePersonaleComponent implements OnInit {
       },
       clearable: true,
       inputClasses: '',
+      autoCapitalize: 'characters',
     },
     prenume: {
       placeholder: 'Ion',
@@ -43,6 +44,7 @@ export class DatePersonaleComponent implements OnInit {
       },
       clearable: true,
       inputClasses: '',
+      autoCapitalize: 'characters',
     },
     cnp: {
       placeholder: '1234567890123',
