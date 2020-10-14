61ce669c138626fc8d4def99ac8f625933b67c69
maxciceu <maximilian.ciceu@softescu.com>
2020-10-14 09:38:36 +0300

    Also add it to the registration

--- omn/src/app/registration/reg-input-sms/reg-input-sms.component.ts
+++ omn/src/app/registration/reg-input-sms/reg-input-sms.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../core/services/auth/auth.service';
 import { AfterViewInit, Component, HostBinding, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -5,13 +6,13 @@ import { CustomTimersService } from 'src/app/core/services/custom-timers/custom-
 import { IonInputConfig } from 'src/app/shared/models/component/ion-input-config';
 import { RegistrationService } from './../../core/services/auth/registration.service';
 
-@Component( {
+@Component({
   selector: 'app-reg-input-sms',
   templateUrl: './reg-input-sms.component.html',
-  styleUrls: [ './reg-input-sms.component.scss' ],
-} )
+  styleUrls: ['./reg-input-sms.component.scss'],
+})
 export class RegInputSmsComponent implements OnInit, AfterViewInit {
-  @HostBinding( 'class' ) color = 'ion-color-white-page';
+  @HostBinding('class') color = 'ion-color-white-page';
   min = '00';
   sec: any = 59;
   phoneNumber = null;
@@ -24,8 +25,8 @@ export class RegInputSmsComponent implements OnInit, AfterViewInit {
     private route: ActivatedRoute,
     private router: Router,
     private timers: CustomTimersService,
-    private formBuilder: FormBuilder,
-    private regService: RegistrationService,
+    private auth: AuthService,
+    private regService: RegistrationService
   ) {
     this.checkUserObj();
   }
@@ -39,7 +40,7 @@ export class RegInputSmsComponent implements OnInit, AfterViewInit {
       !this.regService.getuserObj?.phoneNumber ||
       !this.regService.getuserObj?.userName
     ) {
-      this.router.navigate( [ '/registration' ] );
+      this.router.navigate(['/registration']);
     }
   }
 
@@ -48,38 +49,41 @@ export class RegInputSmsComponent implements OnInit, AfterViewInit {
   }
 
   startTimer() {
-    this.timers.buildTimer( 59 ).subscribe( ( time: number ) => {
+    this.timers.buildTimer(59).subscribe((time: number) => {
       this.sec = time;
-    } );
+    });
   }
 
   resendSMS() {
     this.regService
-      .RegisterPhoneNumber( this.regService.getuserObj.phoneNumber )
+      .RegisterPhoneNumber(this.regService.getuserObj.phoneNumber)
       .subscribe(
-        ( data ) => {
+        (data) => {
           this.startTimer();
         },
-        ( err ) => err
+        (err) => err
       );
   }
 
-  verifyDigit( passForm: FormGroup ) {
+  verifyDigit(passForm: FormGroup) {
     this.regService
-      .ConfirmPhoneNumber( passForm.get( 'passcode' ).value )
+      .ConfirmPhoneNumber(passForm.get('passcode').value)
       .subscribe(
-        ( data ) => {
-          this.router.navigate( [ 'registration/notice' ] );
+        (data) => {
+          this.router.navigate(['registration/notice']);
         },
-        ( err ) => {
+        (err) => {
           passForm.reset();
           this.InvalidCode = err.error;
         }
       );
   }
 
-  clearErr( _ ) {
+  clearErr(_) {
     this.InvalidCode = null;
   }
 
+  doLogout() {
+    this.auth.doLogout();
+  }
 }
