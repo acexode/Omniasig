e3642fe8784e4da30e13d05dd45c5642ecc4a0e3
Okeke Julius Chekwube <julius.ochekwube@softescu.com>
2020-10-19 17:43:48 +0100

    Update to select front camera for capture photo and back camera for the capture docs2

--- omn/src/app/biometrics/services/photo.service.ts
+++ omn/src/app/biometrics/services/photo.service.ts
@@ -21,9 +21,8 @@ export class PhotoService {
 
   constructor(private reqS: RequestService, private camera: Camera) {}
 
-  public async addNewToGallery(newF = false, url) {
-    this.options.cameraDirection = (url === 'capture-docs') ? this.camera.Direction.BACK : this.options.cameraDirection;
-    console.log('DIRECTION: ', this.options.cameraDirection, url);
+  public async addNewToGallery(newF = false, route) {
+    if(route === 'capture-docs') this.options.cameraDirection = this.camera.Direction.BACK;
     const nOptions = {
       ...this.options,
       ...{
