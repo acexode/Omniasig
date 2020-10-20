71826a1606c207aa9c52ad1c9f62411cd143071a
Okeke Julius Chekwube <julius.ochekwube@softescu.com>
2020-10-20 14:15:08 +0100

    Update the expiry date in the homepage

--- omn/src/app/modules/policy/services/policy-data.service.ts
+++ omn/src/app/modules/policy/services/policy-data.service.ts
@@ -320,7 +320,7 @@ export class PolicyDataService {
       padPolicyDocumentId: offer.padPolicyDocumentId,
     };
     if (typeId === 'AMPLUS' || typeId === 'AMPLUS_PAD') {
-      offerObj.expiry = get(offer, 'offerValidUntilDate', '');
+      offerObj.expiry = get(offer, 'offerExpireDate', '');
       offerObj.policy.dates.to = get(offer, 'offerExpireDate', '');
       const isGold = get(offer, 'isGold', false);
       const isVip = get(offer, 'isVip', false);
