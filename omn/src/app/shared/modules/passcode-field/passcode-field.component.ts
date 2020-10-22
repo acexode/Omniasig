fce967b2fe8a5dcefd978974c1b91b5b3ce49823
Ereyomi Oluwaseyi Samuel <ereyomi.oluwaseyi@softescu.com>
2020-10-22 15:54:34 +0100

    refactor: passcode field doreset c

--- omn/src/app/shared/modules/passcode-field/passcode-field.component.ts
+++ omn/src/app/shared/modules/passcode-field/passcode-field.component.ts
@@ -10,13 +10,13 @@ import {
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonInput } from '@ionic/angular';
 
-@Component({
+@Component( {
   selector: 'app-passcode-field',
   templateUrl: './passcode-field.component.html',
-  styleUrls: ['./passcode-field.component.scss'],
-})
+  styleUrls: [ './passcode-field.component.scss' ],
+} )
 export class PasscodeFieldComponent implements OnInit, AfterViewInit {
-  @ViewChild('inputField', { static: true }) inputField: IonInput;
+  @ViewChild( 'inputField', { static: true } ) inputField: IonInput;
   digitsLength = 0;
   @Input() errorLogin: string | null | boolean;
   @Input() busy: boolean;
@@ -25,10 +25,9 @@ export class PasscodeFieldComponent implements OnInit, AfterViewInit {
   @Output() doClearErr: EventEmitter<any> = new EventEmitter();
   @Output() doDigitLength: EventEmitter<number> = new EventEmitter();
   savePin = [];
-  constructor(private formBuilder: FormBuilder) {}
+  constructor( private formBuilder: FormBuilder ) { }
 
   ngOnInit() {
-    console.log( this.savePin );
     this.initForm();
   }
 
@@ -43,19 +42,20 @@ export class PasscodeFieldComponent implements OnInit, AfterViewInit {
   }
 
   initForm() {
-    this.passForm = this.formBuilder.group({
+    this.passForm = this.formBuilder.group( {
       passcode: [
         '',
-        [Validators.required, Validators.minLength(6), Validators.maxLength(6)],
+        [ Validators.required, Validators.minLength( 6 ), Validators.maxLength( 6 ) ],
       ],
-    });
+    } );
 
-    this.passForm.valueChanges.subscribe((value) => {
+    this.passForm.valueChanges.subscribe( ( value ) => {
       this.changeInput( value.passcode );
-    });
+    } );
   }
 
-  changeInput(passCode: string) {
+  changeInput( passCode: string ) {
+    console.log( 'passCode: ', passCode );
     this.digitsLength = passCode ? passCode.length : 0;
     this.emitDigitLength();
     /*  clear savePin*/
@@ -63,37 +63,37 @@ export class PasscodeFieldComponent implements OnInit, AfterViewInit {
       this.doResetForSavePin();
     }
     /*  */
-    if (this.digitsLength === 6 && !this.busy) {
+    if ( this.digitsLength === 6 && !this.busy ) {
       this.emitForm();
     }
-    if (this.digitsLength > 6) {
+    if ( this.digitsLength > 6 ) {
       const value = this.passCode.value ? this.passCode.value : '';
       try {
-        this.passCode.setValue(value.substring(0, 6), { emitEvent: false });
-      } catch (e) {}
+        this.passCode.setValue( value.substring( 0, 6 ), { emitEvent: false } );
+      } catch ( e ) { }
     }
     this.clearErr();
   }
 
   get passCode() {
-    return this.passForm.get('passcode');
+    return this.passForm.get( 'passcode' );
   }
   public emitForm(): void {
-    this.doPassForm.emit(this.passForm);
+    this.doPassForm.emit( this.passForm );
   }
 
   public emitDigitLength(): void {
-    this.doDigitLength.emit(this.digitsLength > 6 ? 6 : this.digitsLength);
+    this.doDigitLength.emit( this.digitsLength > 6 ? 6 : this.digitsLength );
   }
 
   clearErr() {
-    if (this.errorLogin != null || typeof this.errorLogin != null) {
-      this.doClearErr.emit(null);
+    if ( this.errorLogin != null || typeof this.errorLogin != null ) {
+      this.doClearErr.emit( null );
     }
   }
 
   clickingInput( pinKey: any ) {
-    switch ( pinKey) {
+    switch ( pinKey ) {
       case 'backIcon':
         this.savePin.pop();
         break;
@@ -103,12 +103,11 @@ export class PasscodeFieldComponent implements OnInit, AfterViewInit {
     }
     this.digitsLength = this.savePin.length;
     if ( this.digitsLength === 6 ) {
-      this.passCode.setValue( this.savePin.join(''));
+      this.passCode.setValue( this.savePin.join( '' ) );
     }
   }
-  doResetForSavePin(){
+  doResetForSavePin() {
     this.savePin = [];
     this.digitsLength = 0;
-    this.clearErr();
   }
 }
