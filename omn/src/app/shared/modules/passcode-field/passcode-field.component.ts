9527ce466e95a67248f53d61899df6e0112ed995
Max Ciceu <maximilian.ciceu@softescu.com>
2020-10-23 12:00:51 +0300

    Update UI

--- omn/src/app/shared/modules/passcode-field/passcode-field.component.ts
+++ omn/src/app/shared/modules/passcode-field/passcode-field.component.ts
@@ -1,5 +1,4 @@
 import {
-  AfterViewInit,
   Component,
   EventEmitter,
   Input,
@@ -10,13 +9,13 @@ import {
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonInput } from '@ionic/angular';
 
-@Component( {
+@Component({
   selector: 'app-passcode-field',
   templateUrl: './passcode-field.component.html',
-  styleUrls: [ './passcode-field.component.scss' ],
-} )
-export class PasscodeFieldComponent implements OnInit, AfterViewInit {
-  @ViewChild( 'inputField', { static: true } ) inputField: IonInput;
+  styleUrls: ['./passcode-field.component.scss'],
+})
+export class PasscodeFieldComponent implements OnInit {
+  @ViewChild('inputField', { static: true }) inputField: IonInput;
   digitsLength = 0;
   @Input() errorLogin: string | null | boolean;
   @Input() busy: boolean;
@@ -25,86 +24,76 @@ export class PasscodeFieldComponent implements OnInit, AfterViewInit {
   @Output() doClearErr: EventEmitter<any> = new EventEmitter();
   @Output() doDigitLength: EventEmitter<number> = new EventEmitter();
   savePin = [];
-  constructor( private formBuilder: FormBuilder ) { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.initForm();
   }
 
-  clickInput() {
-    this.inputField.setFocus();
-  }
-
-  ngAfterViewInit() {
-    /* setTimeout(() => {
-      this.clickInput();
-    }, 200); */
-  }
-
   initForm() {
-    this.passForm = this.formBuilder.group( {
+    this.passForm = this.formBuilder.group({
       passcode: [
         '',
-        [ Validators.required, Validators.minLength( 6 ), Validators.maxLength( 6 ) ],
+        [Validators.required, Validators.minLength(6), Validators.maxLength(6)],
       ],
-    } );
+    });
 
-    this.passForm.valueChanges.subscribe( ( value ) => {
-      this.changeInput( value.passcode );
-    } );
+    this.passForm.valueChanges.subscribe((value) => {
+      this.changeInput(value.passcode);
+    });
   }
 
-  changeInput( passCode: string ) {
+  changeInput(passCode: string) {
     this.digitsLength = passCode ? passCode.length : 0;
     this.emitDigitLength();
     /*  clear savePin*/
-    if ( passCode === null && this.savePin.length > 5 ) {
+    if (passCode === null && this.savePin.length > 5) {
       this.doResetForSavePin();
     }
     /*  */
-    if ( this.digitsLength === 6 && !this.busy ) {
+    if (this.digitsLength === 6 && !this.busy) {
       this.emitForm();
     }
-    if ( this.digitsLength > 6 ) {
+    if (this.digitsLength > 6) {
       const value = this.passCode.value ? this.passCode.value : '';
       try {
-        this.passCode.setValue( value.substring( 0, 6 ), { emitEvent: false } );
-      } catch ( e ) { }
+        this.passCode.setValue(value.substring(0, 6), { emitEvent: false });
+      } catch (e) { }
     }
     this.clearErr();
   }
 
   get passCode() {
-    return this.passForm.get( 'passcode' );
+    return this.passForm.get('passcode');
   }
   public emitForm(): void {
-    this.doPassForm.emit( this.passForm );
+    this.doPassForm.emit(this.passForm);
   }
 
   public emitDigitLength(): void {
-    this.doDigitLength.emit( this.digitsLength > 6 ? 6 : this.digitsLength );
+    this.doDigitLength.emit(this.digitsLength > 6 ? 6 : this.digitsLength);
   }
 
   clearErr() {
-    if ( this.errorLogin != null || typeof this.errorLogin != null ) {
-      this.doClearErr.emit( null );
+    if (this.errorLogin != null || typeof this.errorLogin != null) {
+      this.doClearErr.emit(null);
     }
   }
 
-  clickingInput( pinKey: any ) {
-    switch ( pinKey ) {
+  clickingInput(pinKey: any) {
+    switch (pinKey) {
       case 'backIcon':
-        if ( this.savePin.length > 0 ) {
+        if (this.savePin.length > 0) {
           this.savePin.pop();
         }
         break;
       default:
-        this.savePin.push( pinKey );
+        this.savePin.push(pinKey);
         break;
     }
     this.digitsLength = this.savePin.length;
-    if ( this.digitsLength === 6 ) {
-      this.passCode.setValue( this.savePin.join( '' ) );
+    if (this.digitsLength === 6) {
+      this.passCode.setValue(this.savePin.join(''), { emitEvent: true });
     }
   }
   doResetForSavePin() {
